cb5e03c8e07b22c37142dd4070d71c51fac26131
ytw9278 <18553208281@163.com>
2020-12-11 21:25:43 +0800

    99

--- yy.py
+++ yy.py
@@ -1,18 +1,17 @@
 import random
-ranlist = ['python','c','ruby','cisco']
-n1=random.choice(ranlist)
-n2=random.sample(ranlist,2)
-print(n1,n2,sep='\n');
 
-ip1 = random.randint(0,255)
-ip2 = random.randint(0,255)
-ip3= random.randint(0,255)
-ip4 =random.randint(0,255)
+ranlist = ['python', 'c', 'ruby', 'cisco']
+n1 = random.choice(ranlist)
+n2 = random.sample(ranlist, 2)
+print(n1, n2, sep='\n')
+
+ip1 = random.randint(0, 255)
+ip2 = random.randint(0, 255)
+ip3 = random.randint(0, 255)
+ip4 = random.randint(0, 255)
 random_ip = str(ip1) + '.' + str(ip2) + '.' + str(ip3) + '.' + str(ip4)
 print(random_ip)
 
 s = 'fuck tangwei'
-z=s.upper()
+z = s.upper()
 print(z)
-
-
