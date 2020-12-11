74c1cca17bc5d097d02221df4967c733e86ee1e8
ytw9278 <18553208281@163.com>
2020-12-11 11:54:54 +0800

    字符串练习

--- yy.py
+++ yy.py
@@ -4,9 +4,13 @@ n1=random.choice(ranlist)
 n2=random.sample(ranlist,2)
 print(n1,n2,sep='\n');
 
-ip1=random.randint(0,255)
-ip2=random.randint(0,255)
-ip3=random.randint(0,255)
-ip4=random.randint(0,255)
+ip1 = random.randint(0,255)
+ip2 = random.randint(0,255)
+ip3= random.randint(0,255)
+ip4 =random.randint(0,255)
 random_ip = str(ip1) + '.' + str(ip2) + '.' + str(ip3) + '.' + str(ip4)
 print(random_ip)
+
+s = 'fuck tangwei'
+z=s.upper()
+print(z)
