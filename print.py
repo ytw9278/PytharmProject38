2958a8ee408c27a28bc0e1ba77a6784ebe0c9237
ytw9278 <18553208281@163.com>
2020-12-11 17:10:02 +0800

    hello，ljust

---
+++ print.py
@@ -0,0 +1,12 @@
+de1 = 'security'
+de2 = 'python'
+de_name1 = 'tangwei'
+de_name2 = 'yuxiaobin'
+age1 = '36'
+age2 = '39'
+
+line1 = de1.ljust(10) + de_name1.ljust(10) + age1.ljust(5)
+length = len(line1)
+print('='*length)
+print(line1)
+print('='*length)
