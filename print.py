d234865e79396acee11835903ce3d91901b4c278
ytw9278 <18553208281@163.com>
2020-12-11 17:34:52 +0800

    字符串对其打印

--- print.py
+++ print.py
@@ -10,11 +10,12 @@ d3 = '年龄'
 
 line1 = d1+':' + format(de1,'<20') + d2+':' + format(de_name1,'<20')  + d3 +':' + format(age1,'<20')
 line2 = d1+':' + de2.ljust(20) + d2+':' + de_name2.ljust(20) + d3+':' + age2.ljust(10)
-
+line3 = d1+':' + format(de1,'*<20') + d2+':' + format(de_name1,'<20')  + d3 +':' + format(age1,'<20')
 # line1 = d1+':' + de1.ljust(20) + d2+':' + de_name1.ljust(20) + d3 +':' + age1.ljust(10)
 # line2 = d1+':' + de2.ljust(20) + d2+':' + de_name2.ljust(20) + d3+':' + age2.ljust(10)
 length = len(line1)
 print('='*length)
 print(line1)
 print(line2)
+print(line3)
 print('='*length)
