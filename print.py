cbc9a83694bd75d3b12f39fb705e36774310d08f
ytw9278 <18553208281@163.com>
2020-12-11 17:12:35 +0800

    hello，ljust,success

--- print.py
+++ print.py
@@ -5,7 +5,7 @@ de_name2 = 'yuxiaobin'
 age1 = '36'
 age2 = '39'
 
-line1 = de1.ljust(10) + de_name1.ljust(10) + age1.ljust(5)
+line1 = 'de1:'+ de1.ljust(20) + 'dename1:' +de_name1.ljust(20) + 'age:'+ age1.ljust(10)
 length = len(line1)
 print('='*length)
 print(line1)
