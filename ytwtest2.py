01f00cb94bb245852c8fb6e7576208443c648aa1
ytw9278 <18553208281@163.com>
2020-12-16 21:37:23 +0800

    985

--- ytwtest2.py
+++ ytwtest2.py
@@ -1,4 +1,4 @@
-import os
+# import os
 # print(os.getcwd())  #存放的文件夹
 # print(os.path.join('e:\\officepython', 'chap2'))
 # print(os.listdir())
@@ -18,12 +18,20 @@ import os
 # print(os.walk('/root'))
 #
 
-lst = os.listdir()
+# lst = os.listdir()
 # print(lst)
 #从文件中查找结尾为py的文件
 # for i in lst:
 #     if i.endswith('py'):
 #         print(i)
-for i in lst:
-    if i.startswith('123') and i.endswith('.txt'):
-        print(i)
+# for i in lst:
+#     if i.startswith('123') and i.endswith('.txt'):
+#         print(i)
+
+import glob
+# print(glob.glob(('*.py'))) #以.py结束的文件
+# print(glob.glob('y*.py'))
+# print(glob.glob('**/*.py', recursive=True)) #标示单是任意层文件夹
+
+import fnmatch
+print(fnmatch.fnmatch('ytwwww.py', 'yt?.py'))
