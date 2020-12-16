9c3120f197837f7ff10f397f91522b0d7e9309d0
ytw9278 <18553208281@163.com>
2020-12-16 08:56:47 +0800

    985

--- ppppp.py
+++ ppppp.py
@@ -23,5 +23,54 @@
 
 
 
-for i in range(1,101):    #左开右关  遍历
-    print(i)
+# for i in range(1,101):    #左开右关  遍历
+#     print(i)
+
+#
+# for ch in 'Python':
+#     print(ch)
+
+# a = 1
+# while a <= 1000:
+#     print(a)
+#     a = a+1
+
+
+# while True:
+#     answer = input('你的腿怎么样？能坚持吗？')
+#     if answer=="y":
+#         print('加油!')
+#     else:
+#         print('不能，退赛！')
+#         break
+
+
+# circle = 0
+# for i in range(1, 12):
+#     answer = input('能跑吗？')
+#     if answer != 'y':
+#         continue
+#     circle+=1
+# print('一共跑了', circle, '圈')
+
+
+lst = [50, 51, 52, 53, 54]
+# print(lst)
+# print(lst[3])
+# print(lst[-2])
+# lst.append(100)
+# print(lst)
+# lst[1]=88
+# print(lst)
+# lst.remove(54)
+# print(lst)
+# for i in range(0, len(lst)):
+#     print(lst[i])
+# for item in lst:
+#     print(item)
+
+# lst = [50, 51, 52, 53, 54]
+# lst1 = lst[1:]
+# lst2 = lst[1:3]
+# print(lst1)
+# print(lst2)
